cfdbca267782f932cd1c859c24af0bf1954287e0
Gmailvicente04 <vicente.oswald@gmail.com>
2025-03-02 19:59:56 -0500

    "Task 1 - Created Book Class"

---
+++ cc_11.js
@@ -0,0 +1,19 @@
+// Task 1: Creating a Book Class
+class Book {
+    constructor(title, author, isbn, copies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
+    }
+
+    getDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`;
+    }
+
+    updateCopies(quantity) {
+        this.copies += quantity;
+    }
+}
+const book1 = new Book('The Alchemist', 'Paulo Coelho', '9780062315007', 5);
+console.log(book1.getDetails());
